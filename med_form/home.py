efbd9947980a33d37100bab198f78ab0d2b7a4ed
antonioorichard <antoniorichardhc@gmail.com>
2025-04-29 18:30:39 -0300

    corrindo o arquivo home.py para versão certa.

--- med_form/home.py
+++ med_form/home.py
@@ -1,44 +1,56 @@
 import streamlit as st
-
+import requests
 st.set_page_config(
     page_title = "Formulário médico"
     #page_icon = ""
 
 )
 
-api_gateway_endpoint = "" # Substitua pela URL correta do API Gateway
+api_gateway_endpoint = "https://api-flask-ds.onrender.com/" # Substitua pela URL correta do API Gateway
 
 with st.form( key = 'form1'):
     col1, col2, col3 = st.columns([3,2,1])
     with col1:
-            raiox_res  = st.text_input("raiox_res")
+            ave_suino  = st.text_input("ave_suino")
     with col2:
-            fnt_in_cov = st.text_input("fnt_in_cov")
+            garganta    = st.text_input("garganta")
     with col3:
-            delta_uti  = st.text_input("delta_uti")
+            perd_pala  = st.text_input("perd_pala")
     with col1:
-            amostra    = st.text_input("amostra")
+            febre = st.text_input("febre")
     with col2:
-            hospital   = st.text_input("hospital")
+            dispneia   = st.text_input("dispneia")
+    with col3:
+            diarreia   = st.text_input("diarreia")
+    with col1:
+            tosse  = st.text_input("tosse")
+    with col2:
+            perd_olft  = st.text_input("perd_olft")
+    with col3:
+            vomito     = st.text_input("vomito")
 #    st.form_submit_button()
-
-        if st.form_submit_button():
-                # Preparar os dados para a chamada do API Gateway
-                input_data = {
-                        'raiox_res' : raiox_res, 
-                        'fnt_in_cov': fnt_in_cov,
-                        'delta_uti' : delta_uti, 
-                        'amostra'   : amostra, 
-                        'hospital'  : hospital
-                }
-
-                # Enviar a requisição POST para o API Gateway
-                response = requests.post( api_gateway_endpoint, json = input_data)
-
-                # Tratar a resposta do API Gateway 
-                if response.status_code == 200:
-                        output_data = response.json()
-                        st.write("Resultado da inferência: ")
-                        st.write( output_data )
-                else: 
-                        st.write( "Erro ao chamar o API Gateway.")
+    if st.form_submit_button():
+            # Preparar os dados para a chamada do API Gateway
+            input_data = {
+                    'ave_suino' : ave_suino, 
+                    'febre'     : febre,
+                    'tosse'     : tosse,
+                    'garganta'  : garganta, 
+                    'dispneia'  : dispneia, 
+                    'perd_olft' : perd_olft,
+                    'perd_pala' : perd_pala,
+                    'diarreia'  : diarreia, 
+                    'vomito'    : vomito 
+                   
+            }
+            # Enviar a requisição POST para o API Gateway
+            response = requests.post( api_gateway_endpoint, json = input_data)
+    
+            # Tratar a resposta do API Gateway 
+            if response.status_code == 200:
+                # Vamos receber a resposta, do tipo string, então vamos usar .text
+                    output_data = response.text
+                    st.write("Resultado da inferência: ")
+                    st.write( output_data )
+            else: 
+                    st.write( "Erro ao chamar o API Gateway.")
